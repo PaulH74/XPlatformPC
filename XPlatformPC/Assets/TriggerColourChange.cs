187d23abf0aba6bf6eab30007448f5521ae9f331
Paul <iamtheodo@gmail.com>
2020-10-15 17:03:34 +1100

    RPC Colour Change Added

---
+++ XPlatformPC/Assets/TriggerColourChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace XPlatformPC
+{
+    public class TriggerColourChange : MonoBehaviourPun
+    {
+        private Color _OriginColour;
+        private Color _RedColour;
+        private Material _ObjectMaterial;
+
+        private void Awake()
+        {
+            _ObjectMaterial = GetComponent<MeshRenderer>().material;
+            _OriginColour = _ObjectMaterial.color;
+            _RedColour = Color.red;
+        }
+
+        [PunRPC]
+        private void ChangeColour(bool change)
+        {
+            if (change)
+            {
+                _ObjectMaterial.color = _RedColour;
+            }
+            else
+            {
+                _ObjectMaterial.color = _OriginColour;
+            }
+        }
+
+        public void TriggerColour(bool triggerChange)
+        {
+            photonView.RPC("ChangeColour", RpcTarget.All, triggerChange);
+        }
+    }
+}
